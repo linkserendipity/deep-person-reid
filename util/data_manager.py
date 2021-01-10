a256800bde87b8567b44c2a6ca8d091021bfae11
unknown <462710683@qq.com>
2021-01-10 15:52:51 +0800

    util

---
+++ util/data_manager.py
@@ -0,0 +1,46 @@
+from __future__ import print_function, absolute_import
+import os
+import os.path as osp
+import numpy as np
+
+from utils import mkdir_if_missing(),  write_json, read_json
+
+from Ipython import embed
+
+class Market1501(object):
+    """
+    Market1501
+    Reference:
+    Zheng et al. Scalable Person Re-identification: A Benchmark. ICCV 2015.
+    URL: http://www.liangzheng.org/Project/project_reid.html
+    
+    Dataset statistics:
+    # identities: 1501 (+1 for background)
+    # images: 12936 (train) + 3368 (query) + 15913 (gallery)
+    """
+    dataset_dir = 'market1501'
+
+    def __init__(self, root = 'data', **kwargs):
+        self.dataset_dir = osp.join(root, self.dataset_dir)
+        self.train_dir = osp.join(self.dataset_dir, 'bounding_box_train')
+        self.query_dir = osp.join(self.dataset_dir, 'query')
+        self.gallery_dir = osp.join(self.dataset_dir, 'bounding_box_test')
+
+        self._check_before_run()
+
+    def _check_before_run(self):
+        if not osp.exists(self.dataset_dir):
+            raise RuntimeError("'{}' is not available".format(self.dataset_dir))
+        if not osp.exists(self.train_dir):
+            raise RuntimeError("'{}' is not available".format(self.train_dir))
+        if not osp.exists(self.query_dir):
+            raise RuntimeError("'{}' is not available".format(self.query_dir))
+        if not osp.exists(self.gallery_dir):
+            raise RuntimeError("'{}' is not available".format(self.gallery_dir))
+
+if __name__ = '__main__':
+    data = Market1501(root = '/home/ls/data')
+
+
+
+
