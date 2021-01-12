4a6bc127e0646a993d11aba6a74fc33eb594613e
unknown <462710683@qq.com>
2021-01-12 12:39:02 +0800

    res

--- models/res.py
+++ models/res.py
@@ -11,12 +11,12 @@ class ResNet50(nn.Module):
             super(ResNet50, self).__init__()
             resnet50 = torchvision.models.resnet50(pretrained=True)
             # resnet50 = torchvision.models.resnet50(pretraind=True)
-            self.base = nn.Sequential(*list(resnet50.children())[:-2]) #
+            self.base = nn.Sequential(*list(resnet50.children())[:-2])  #
             self.classifier = nn.Linear(2048, num_classes)
             
     def forward(self, x):
-        x =self.base(x)      
-        x = F.avg_pool2d(x, x.size()[:2])
+        x = self.base(x)     
+        x = F.avg_pool2d(x, x.size()[2:])
         f = x.view(x.size(0), -1)
         # normalize
         # f = 1.*f / (torch.norm(f, 2, dim=-1, keepdim=True).expand_as(f) + 1e-12)
