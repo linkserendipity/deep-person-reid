4770ddf9b925eca887138e27c9adec82bd56974c
unknown <462710683@qq.com>
2021-01-12 12:35:09 +0800

    res

--- models/res.py
+++ models/res.py
@@ -16,11 +16,19 @@ class ResNet50(nn.Module):
             
     def forward(self, x):
         x =self.base(x)      
-        embed()
+        x = F.avg_pool2d(x, x.size()[:2])
+        f = x.view(x.size(0), -1)
+        # normalize
+        # f = 1.*f / (torch.norm(f, 2, dim=-1, keepdim=True).expand_as(f) + 1e-12)
 
-
+        if not self.training:
+            return f
+        y = classifier(f)
+        return y
+        
 
 if __name__ == "__main__":
     model = ResNet50(num_classes = 751)    
     imgs = torch.Tensor(32, 3, 256, 128)
     f = model(imgs)
+    embed()
