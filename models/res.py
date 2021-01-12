35a2bdfba25324347b8298ecf2f1dca56d9c0839
unknown <462710683@qq.com>
2021-01-12 11:16:17 +0800

    res

---
+++ models/res.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+import torchvision
+from IPython import embed
+
+class ResNet50(nn.Module):
+    def __init__(self, num_classes, loss={'softmax, metric'}, **kwargs):
+            super(ResNet50, self).__init__()
+            resnet50 = torchvision.models.resnet50(pretraind = True)
+            embed()
+
+if __name__ == "__main__":
+    model = Resnet50(num_classes = 751)    
