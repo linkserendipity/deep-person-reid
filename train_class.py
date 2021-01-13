ed1e9fac81906c958bb372e86c61e68a35068d10
linkserendipity <462710683@qq.com>
2021-01-13 16:14:01 +0800

    sh

--- train_class.py
+++ train_class.py
@@ -125,7 +125,8 @@ def test(model, queryloader, galleryloader, use_gpu, ranks=[1, 5, 10, 20]):
     distmat = torch.pow(qf, 2).sum(dim=1, keepdim=True).expand(m, n) + \
               torch.pow(gf, 2).sum(dim=1, keepdim=True).expand(n, m).t()
     # distmat.addmm_(1, -2, qf, gf.t())  # ???!!! warning
-    distmat.addmm_(qf, gf.t(), 1, -2)  # addmm_(Tensor mat1, Tensor mat2, *, Number beta, Number alpha)
+    torch.addmm_(qf, gf.t(), distmat, 1, -2)  # addmm_() takes 2 positional arguments but 4 were given
+    # addmm_(Tensor mat1, Tensor mat2, *, Number beta, Number alpha)
     distmat = distmat.numpy()
     # distmat.addmm(mat1, mat2, *, beta=1, alpha=-2)
     print("Computing CMC and mAP")
