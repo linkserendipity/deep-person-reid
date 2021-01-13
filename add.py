c56dbf92f346c03f538a7ec268e929542200650f
linkserendipity <462710683@qq.com>
2021-01-13 21:15:39 +0800

    sh

---
+++ add.py
@@ -0,0 +1,10 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.backends.cudnn as cudnn  
+from torch.utils.data import DataLoader
+from torch.optim import lr_scheduler
+
+
+
+# t1 = torch.Tensor(data)
--- train_class.py
+++ train_class.py
@@ -124,7 +124,7 @@ def test(model, queryloader, galleryloader, use_gpu, ranks=[1, 5, 10, 20]):
     m, n = qf.size(0), gf.size(0)
     distmat = torch.pow(qf, 2).sum(dim=1, keepdim=True).expand(m, n) + \
               torch.pow(gf, 2).sum(dim=1, keepdim=True).expand(n, m).t()
-    distmat.addmm_(1, -2, qf, gf.t())
+    distmat.addmm_(qf, gf.t(), 1, -2)
     distmat = distmat.numpy()
 
     # m, n = qf.size(0), gf.size(0)
