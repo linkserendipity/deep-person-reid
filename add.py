444744912a962f348307da200b0f685346210a56
linkserendipity <462710683@qq.com>
2021-01-13 21:25:54 +0800

    sh

--- add.py
+++ add.py
@@ -1,10 +1,16 @@
 import torch
-import numpy as np
-import torch.nn as nn
-import torch.backends.cudnn as cudnn  
-from torch.utils.data import DataLoader
-from torch.optim import lr_scheduler
+# import numpy as np
 
+M = torch.zeros(2, 3) + 1
+m1 = torch.zeros(2, 3) + 2
+m2 = torch.zeros(3, 3) + 10
 
+A = torch.addmm(M, m1, m2)
+print("A:\n", A)
 
-# t1 = torch.Tensor(data)
+B = torch.addmm(M, m1, m2, beta=1, alpha=-2)
+print("\nB 1-2:\n", B)
+print("\nM:\n", M)
+C = M.addmm_(m1, m2, beta=1, alpha=-2)
+print("\nC 1-2:\n", C)
+print("\nM 1-2:\n", M)
