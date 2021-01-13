e99a18fceddc76e73d38299f690c25b300eaa7c1
linkserendipity <462710683@qq.com>
2021-01-13 21:38:02 +0800

    sh

--- add.py
+++ add.py
@@ -12,5 +12,8 @@ B = torch.addmm(M, m1, m2, beta=1, alpha=-2)
 print("\nB 1-2:\n", B)
 print("\nM:\n", M)
 C = M.addmm_(m1, m2, beta=1, alpha=-2)
+# C = M.addmm_(m1, m2, 1, -2) will cause a error, so beta and alpha must be allocated precisely 
+# TypeError: addmm_() takes 2 positional arguments but 4 were given
+
 print("\nC 1-2:\n", C)
 print("\nM 1-2:\n", M)
