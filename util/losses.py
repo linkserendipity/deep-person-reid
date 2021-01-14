30f3582ac4ffbfbdca6fa94b167e5778f45c3a67
linkserendipity <462710683@qq.com>
2021-01-14 12:02:10 +0800

    sh

--- util/losses.py
+++ util/losses.py
@@ -81,14 +81,46 @@ class TripletLoss(nn.Module):
             inputs: feature matrix with shape (batch_size, feat_dim) 32*2048
             targets: ground truth labels with shape (num_classes)
         """
-        n = inputs.size(0)
+        # n = inputs.size(0)
 
-        # (a-b)^2 =a^2-2ab+b^2
-        # ([1,2,3]*[1,2,3]).sum()=[1,4,9].sum()=14  np.array([1,4,9]).sum(dim=1)
+        # # (a-b)^2 =a^2-2ab+b^2
+        # # ([1,2,3]*[1,2,3]).sum()=[1,4,9].sum()=14  np.array([1,4,9]).sum(dim=1)
+        # # Compute pairwise distance, replace by the official when merged
+        # dist = torch.pow(inputs, 2).sum(dim=1, keepdim=True).expand(n, n)  # inputs.^2 sumup then expand to 32*32
+        # dist = dist + dist.t()
+        # dist.addmm_(inputs, inputs.t(), beta=1, alpha=-2)
+        # dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
+        # # For each anchor, find the hardest positive and negative
+        # mask = targets.expand(n, n).eq(targets.expand(n, n).t())
+        # dist_ap, dist_an = [], []
+        # for i in range(n):
+        #     dist_ap.append(dist[i][mask[i]].max().unsqueeze(0))
+        #     dist_an.append(dist[i][mask[i] == 0].min().unsqueeze(0))
+
+        # dist_ap = torch.cat(dist_ap)
+        # dist_an = torch.cat(dist_an)
+        # # Compute ranking hinge loss
+        # y = torch.ones_like(dist_an)
+        # loss = self.ranking_loss(dist_an, dist_ap, y)
+        # return loss
+
+
+    def __init__(self, margin=0.3):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+        self.ranking_loss = nn.MarginRankingLoss(margin=margin)
+
+    def forward(self, inputs, targets):
+        """
+        Args:
+            inputs: feature matrix with shape (batch_size, feat_dim)
+            targets: ground truth labels with shape (num_classes)
+        """
+        n = inputs.size(0)
         # Compute pairwise distance, replace by the official when merged
-        dist = torch.pow(inputs, 2).sum(dim=1, keepdim=True).expand(n, n)  # inputs.^2 sumup then expand to 32*32
+        dist = torch.pow(inputs, 2).sum(dim=1, keepdim=True).expand(n, n)
         dist = dist + dist.t()
-        dist.addmm_(inputs, inputs.t(), beta=1, alpha=-2)
+        dist.addmm_(1, -2, inputs, inputs.t())
         dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
         # For each anchor, find the hardest positive and negative
         mask = targets.expand(n, n).eq(targets.expand(n, n).t())
@@ -96,13 +128,13 @@ class TripletLoss(nn.Module):
         for i in range(n):
             dist_ap.append(dist[i][mask[i]].max().unsqueeze(0))
             dist_an.append(dist[i][mask[i] == 0].min().unsqueeze(0))
-
         dist_ap = torch.cat(dist_ap)
         dist_an = torch.cat(dist_an)
         # Compute ranking hinge loss
         y = torch.ones_like(dist_an)
         loss = self.ranking_loss(dist_an, dist_ap, y)
         return loss
+
 
 class CenterLoss(nn.Module):
     """Center loss.
